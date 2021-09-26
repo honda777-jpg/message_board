44fb5a7533d9455e1b2dd9e3431b22eff0eea7b5
自分の名前 <alex1@192.168.0.12>
2021-09-26 20:25:11 +0900

    CreateServlet.javaを作成

--- src/controllers/IndexServlet.java
+++ src/controllers/IndexServlet.java
@@ -17,7 +17,7 @@ import utils.DBUtil;
 /**
  * Servlet implementation class IndexServlet
  */
-@WebServlet(name = "IndexServlet", urlPatterns = { "/show" })
+@WebServlet(name = "IndexServlet", urlPatterns = { "/index" })
 public class IndexServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
